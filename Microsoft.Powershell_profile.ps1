6813d66281b02c5495f162700b73cc1e75008cb5
Alok Nayak <csnayak.net1@gmail.com>
2025-09-26 19:52:57 +0530

    Created Microsoft.Powershell_profile.ps1, will update...

---
+++ Microsoft.Powershell_profile.ps1
@@ -0,0 +1,6 @@
+winfetch
+\Users\alokn\selectprompt.ps1
+Import-Module syntax-highlighting
+Set-Alias -Name ls -Value lsd
+# Note: you can remove the fourth line if you dont have lsd.
+# 5th line and this line are not in my pwsh profile...
